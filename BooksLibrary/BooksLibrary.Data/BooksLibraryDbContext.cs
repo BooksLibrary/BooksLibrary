32b6694e66275d57926cf8c72dccea20ee95174c
Nikolay Atanasow <n.atanasow@yahoo.com>
2016-01-21 13:26:27 +0200

    Initial Skeleton for WebForms app with Data project and Model project added.

---
+++ BooksLibrary/BooksLibrary.Data/BooksLibraryDbContext.cs
@@ -0,0 +1,23 @@
+﻿namespace BooksLibrary.Data
+{
+    using BooksLibrary.Models;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class BooksLibraryDbContext : IdentityDbContext<User>
+    {
+        public BooksLibraryDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static BooksLibraryDbContext Create()
+        {
+            return new BooksLibraryDbContext();
+        }
+    }
+}
---
+++ BooksLibrary/BooksLibrary.Web/App_Start/DatabaseConfig.cs
@@ -0,0 +1,16 @@
+﻿namespace BooksLibrary.Web.App_Start
+{
+    using System.Data.Entity;
+
+    using BooksLibrary.Data;
+    using BooksLibrary.Data.Migrations;
+
+    public static class DatabaseConfig
+    {
+        public static void Initialize()
+        {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<BooksLibraryDbContext, Configuration>());
+            BooksLibraryDbContext.Create().Database.Initialize(true);
+        }
+    }
+}
