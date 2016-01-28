f4aaa326239ae4923d1f96c663565e7975a89094
VladimirDimov <vdimov1124@gmail.com>
2016-01-28 13:10:01 +0200

    users admininstration added

---
+++ BooksLibrary/BooksLibrary.Web/Admin/Users/All.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" MasterPageFile="~/Site.Master" CodeBehind="All.aspx.cs" Inherits="BooksLibrary.Web.Admin.Users.All" %>
+
+<asp:Content runat="server" ContentPlaceHolderID="MainContent">
+    <asp:ListView runat="server" ID="UsersListView" ItemType="BooksLibrary.Models.User"
+        DataKeyNames="Id"
+        SelectMethod="UsersListView_GetData">
+        <LayoutTemplate>
+            <table class="table table-striped table-hover col-sm-2">
+                <tr id="itemplaceholder" runat="server"></tr>
+            </table>
+        </LayoutTemplate>
+        <ItemTemplate>
+            <tr>
+                <th><%#: Item.UserName %></th>
+                <th>
+                    <asp:Button runat="server" ID="DeleteButton" Text="Delete" OnClick="UsersListView_DeleteItem" CommandArgument="<%# Item.Id %>" class="btn btn-danger"/></th>
+            </tr>
+        </ItemTemplate>
+    </asp:ListView>
+</asp:Content>
---
+++ BooksLibrary/BooksLibrary.Web/Admin/Users/All.aspx.cs
@@ -0,0 +1,51 @@
+﻿using BooksLibrary.Data;
+using BooksLibrary.Models;
+using ChatSystem.Data.Repository;
+using Error_Handler_Control;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace BooksLibrary.Web.Admin.Users
+{
+    public partial class All : System.Web.UI.Page
+    {
+        private IRepository<User> users = new EfGenericRepository<User>(new BooksLibraryDbContext());
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        // The return type can be changed to IEnumerable, however to support
+        // paging and sorting, the following parameters must be added:
+        //     int maximumRows
+        //     int startRowIndex
+        //     out int totalRowCount
+        //     string sortByExpression
+        public IQueryable UsersListView_GetData()
+        {
+            return this.users.All();
+        }
+
+        // The id parameter name should match the DataKeyNames value set on the control
+        public void UsersListView_DeleteItem(object sender, EventArgs e)
+        {
+            var id = (sender as Button).CommandArgument;
+            try
+            {
+                this.users.Delete(id);
+                this.users.SaveChanges();
+                ErrorSuccessNotifier.AddSuccessMessage("User deleted.");
+                this.DataBind();
+            }
+            catch (Exception ex)
+            {
+                ErrorSuccessNotifier.AddErrorMessage(ex.Message);
+            }
+        }
+    }
+}
